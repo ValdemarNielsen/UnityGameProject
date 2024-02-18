3b7f0f290306e094348986da0dcaaccc41c9332c
seburu <sebynoah@gmail.com>
2024-02-18 15:43:27 +0100

    update in controller and button

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using GameProject.Models; // Import the namespace where Player class is defined
+using GameProject.Services; // Import the namespace where Player class is defined
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Reference to the Player component attached to the player GameObject.
+    public Player player;
+
+    // Speed at which the player moves.
+    public float moveSpeed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Handle player input for movement
+        HandleMovementInput();
+    }
+
+    // Method to handle player movement input
+    void HandleMovementInput()
+    {
+        // Get input from the horizontal and vertical axes
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction based on the input
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+        // Calculate the movement amount
+        Vector2 movementAmount = movementDirection * moveSpeed * Time.deltaTime;
+
+        // Move the player
+        transform.Translate(movementAmount);
     }
 }
--- Assets/Scripts/UI/Buttons/ChestButtonHandler.cs
+++ Assets/Scripts/UI/Buttons/ChestButtonHandler.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using GameProject.Models;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ChestButtonHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // The distance at which the player can interact with the chest.
+    public float interactionDistance = 3f;
+
+    // Reference to the player GameObject. You can assign this in the Unity Editor.
+    public Player player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if the player is close enough to the chest and presses the "E" key.
+        if (Input.GetKeyDown(KeyCode.E) && IsPlayerCloseEnough())
+        {
+            // Call the method to handle the interaction with the chest.
+            InteractWithChest();
+        }
+    }
+
+    // Method to check if the player is close enough to the chest.
+    bool IsPlayerCloseEnough()
+    {
+        // Calculate the distance between the player and the chest.
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        // Return true if the distance is less than or equal to the interaction distance.
+        return distance <= interactionDistance;
+    }
+
+    // Method to handle the interaction with the chest.
+    void InteractWithChest()
+    {
+        // Add your logic here for what should happen when the player interacts with the chest.
+        Debug.Log("Chest interacted!");
+
+        // For example, you could open the chest, play an animation, or trigger some other action.
     }
 }

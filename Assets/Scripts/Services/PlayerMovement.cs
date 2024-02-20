9f71f347febc14e692aa47846a0c7341688054eb
seburu <sebynoah@gmail.com>
2024-02-20 16:37:11 +0100

    Made jump script

--- Assets/Scripts/Services/PlayerMovement.cs
+++ Assets/Scripts/Services/PlayerMovement.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 10f;
+    public float jumpForce = 2.5f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
 
-    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -23,24 +22,11 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        // Jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            if (isGrounded)
-            {
-                Debug.Log("IM GROUNDED");
-            }
-            rb.AddForce(transform.up * jumpForce);
-        }
     }
 
     void FixedUpdate()
     {
         // Apply movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        // Check if the player is grounded
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
-        
     }
 }

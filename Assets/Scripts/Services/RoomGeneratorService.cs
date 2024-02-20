66a5ba9285e10d4342f18950836a69ce85ac414c
Valdemar Orla Nielsen <s215798@dtu.dk>
2024-02-20 17:05:20 +0100

    Starting to make the connection with Room doors and unity scenes. Just the beginning

--- Assets/Scripts/Services/RoomGeneratorService.cs
+++ Assets/Scripts/Services/RoomGeneratorService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 /// RoomGeneratorService.cs (optional): If our rooms require complex initialization or configuration beyond just connecting them with doors 
 /// (e.g., assigning types, generating puzzles, placing items), this service would handle the logic of 
 /// creating and initializing Room instances with those specific attributes.
 namespace GameProject.Services
 {
-    public class RoomGeneratorService
+    public class RoomGeneratorService : MonoBehaviour
     {
         public bool Up { get; set; }
         public bool Down { get; set; }

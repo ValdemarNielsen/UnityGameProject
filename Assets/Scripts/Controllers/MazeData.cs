61a34959b711032161ccd35f9671e0acfb3787f3
Valdemar Orla Nielsen <s215798@dtu.dk>
2024-02-27 14:35:58 +0100

    ?!?!

---
+++ Assets/Scripts/Controllers/MazeData.cs
@@ -0,0 +1,30 @@
+﻿using GameProject.Models;
+using GameProject.Services;
+using RoomManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MazeData : MonoBehaviour
+{
+    public static Maze maze;
+    // Reference to the RoomManager GameObject in the scene
+    public RoomManager roomManager;
+
+    void Start() // Change from Awake to Start
+    {
+        Debug.Log("This is MazeData being called");
+        if (maze == null) // Generate only if not already created
+        {
+            MazeGeneratorService mazeGenerator = new MazeGeneratorService(5);
+            maze = mazeGenerator.GenerateMaze();
+            // Assign scenes to rooms using the RoomManager component
+            roomManager.AssignScenesToRooms(maze);
+            Debug.Log("This is part of the MazeData is also being called");
+
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/RoomManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using GameProject.Models;
+using System.Collections.Generic;
+
+namespace RoomManagement
+{
+    public class RoomManager : MonoBehaviour
+    {
+        // List of our scenes
+        private string[] scenes = { "D", "DL", "L", "R", "RD", "RDL", "RL", "U", "UD", "UDL", "UL", "UR", "URD", "URDL", "URL" }; // Add all our scenes
+
+        // Dictionary to map room layouts to scenes
+        private Dictionary<string, string> layoutToSceneMap = new Dictionary<string, string>();
+
+        // Method to initialize the layout to scene mapping
+        private void InitializeLayoutToSceneMap()
+        {
+            // Add mappings based on room layouts and scenes. Its "RoomLetters", "Unity Scene name" . 
+            layoutToSceneMap.Add("D", "D");
+            layoutToSceneMap.Add("DL", "DL");
+            layoutToSceneMap.Add("L", "L");
+            layoutToSceneMap.Add("R", "R");
+            layoutToSceneMap.Add("RD", "RD");
+            layoutToSceneMap.Add("RDL", "RDL");
+            layoutToSceneMap.Add("RL", "RL");
+            layoutToSceneMap.Add("U", "U");
+            layoutToSceneMap.Add("UD", "UD");
+            layoutToSceneMap.Add("UDL", "UDL");
+            layoutToSceneMap.Add("UL", "UL");
+            layoutToSceneMap.Add("UR", "UR");
+            layoutToSceneMap.Add("URD", "URD");
+            layoutToSceneMap.Add("URDL", "URDL");
+            layoutToSceneMap.Add("URL", "URL");
+            // Add all our mappings
+        }
+
+        // Method to assign scenes to rooms in the maze based on their layout
+        public void AssignScenesToRooms(Maze maze)
+        {
+            InitializeLayoutToSceneMap();
+
+            int size = maze.Rooms.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Room room = maze.Rooms[i, j];
+                    string layout = room.DirectionLetter;
+                    string sceneName = GetSceneForLayout(layout);
+                    room.SceneName = sceneName;
+                }
+            }
+        }
+
+        // Method to get the scene associated with a given room layout
+        private string GetSceneForLayout(string layout)
+        {
+            // Check if the layout exists in the mapping
+            if (layoutToSceneMap.ContainsKey(layout))
+            {
+                return layoutToSceneMap[layout];
+            }
+            else
+            {
+                // If the layout does not exist, assign a random scene. This should not happen.
+                int randomIndex = Random.Range(0, scenes.Length);
+                return scenes[randomIndex];
+            }
+        }
+    }
+
+}

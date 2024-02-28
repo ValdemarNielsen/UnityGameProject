626a682007421d4ca6b46002790f5dbb20448118
Valdemar Nielsen <v.nielsen@live.dk>
2024-02-28 16:59:24 +0100

    small changes to movement

--- Assets/Scripts/Services/PlayerMovement.cs
+++ Assets/Scripts/Services/PlayerMovement.cs
@@ -4,10 +4,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 2.5f;
-
+    public float jumpAmount = 10;
+    private bool isGrounded;
     private Rigidbody2D rb;
-    private Vector2 movement;
-
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +16,12 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {  
-
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("IM GROUNDED");
+            Jump();
+        }
     }
 
     void FixedUpdate()
@@ -32,6 +35,27 @@ public class PlayerMovement : MonoBehaviour
         // Apply movement
         rb.velocity = movement;
 
-       
+        // Check if the player is grounded
+        CheckGrounded();
+
+
+    }
+    void Jump()
+    {
+        // Apply jump force
+        rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.Raycast(rb.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+        if (!isGrounded)
+        {
+            isGrounded = Physics2D.Raycast(rb.position, Vector2.right, 2f, LayerMask.GetMask("Ground"));
+        }
+        if (!isGrounded)
+        {
+            isGrounded = Physics2D.Raycast(rb.position, Vector2.left, 2f, LayerMask.GetMask("Ground"));
+        }
     }
 }

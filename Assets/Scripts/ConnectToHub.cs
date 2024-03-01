a82459040c7d255378f497d9ec2b0f4f146c6269
Henrik97 <henrikwoodgates97@gmail.com>
2024-03-01 10:26:31 +0100

    SignalR class to client hub

---
+++ Assets/Scripts/ConnectToHub.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ConnectToHub : MonoBehaviour
+    {
+
+        private HubConnection hubConnection;
+
+        async void Start()
+        {
+            try
+            {
+                hubConnection = new HubConnectionBuilder()
+                    .WithUrl("https://localhost:7206/client-hub")
+                    .Build();
+
+                await hubConnection.StartAsync();
+                Debug.Log("SignalR Connected!");
+
+                // Register event handlers
+                hubConnection.On<string>("ReceiveMessage", (message) =>
+                {
+                    Debug.Log($"Message from server: {message}");
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"An error occurred while connecting to SignalR: {ex.Message}");
+            }
+        }
+
+        void OnDestroy()
+        {
+            // Properly disconnect when the object is destroyed
+            hubConnection?.StopAsync();
+            hubConnection?.DisposeAsync();
+        }
+    }
+        }
+    }
+}

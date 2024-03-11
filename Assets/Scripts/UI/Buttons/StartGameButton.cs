84174153ac313ee66254bef1e2e9802150b50c56
Valdemar Orla Nielsen <s215798@dtu.dk>
2024-03-11 14:00:39 +0100

    added functionality to the start game button.

---
+++ Assets/Scripts/UI/Buttons/StartGameButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Unity.UI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+namespace Assets.Scripts.UI.Buttons
+{
+    internal class StartGameButton : MonoBehaviour
+      
+    {
+        public Button startGameButton;
+
+        void Start()
+        {
+            Button btn = startGameButton.GetComponent<Button>();
+            btn.onClick.AddListener(TaskOnClick);
+        }
+
+        void TaskOnClick()
+        {
+
+            Debug.Log("You have clicked the button!");
+            SceneManager.LoadScene("URDL");
+        }
+    }
+}

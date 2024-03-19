a653e079c17fc1b7ec71a9b37f8dbf5761883354
seburu <sebynoah@gmail.com>
2024-03-19 21:20:16 +0100

    .

--- Assets/Scripts/Services/EnemySpawn.cs
+++ Assets/Scripts/Services/EnemySpawn.cs
@@ -2,45 +2,26 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject enemy;
     [SerializeField] private int spawnRate;
-    [SerializeField] private bool rememberKilledEnemies = true;
 
-    private string enemyID; // Unique identifier for the enemy
 
-    private void Awake()
+    void Start()
     {
-        // Generate unique ID for the enemy
-        enemyID = EnemyData.Instance.GenerateUniqueID();
 
-        // Check if the enemy is killed and destroy if necessary
-        if (rememberKilledEnemies && EnemyData.Instance.IsEnemyKilled(enemyID))
-        {
-            Destroy(gameObject); // Destroy the EnemySpawn GameObject if the enemy is killed
-        }
-        else if (!EnemyData.Instance.IsEnemyKilled(enemyID) && GetRandomPercentage() < spawnRate)
-        {
-            SpawnEnemy(); // Spawn the enemy if it's not killed
-        }
-    }
+        int[] playerPosition = GameManager.GetPlayerPosition();
 
-    private void SpawnEnemy()
-    {
-        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        if (GetRandomPercentage() < spawnRate && !GameManager.HasVisited(playerPosition[0], playerPosition[1]))
+        {
+            Destroy(enemy);
+        }
     }
 
     private int GetRandomPercentage()
     {
         return Random.Range(0, 100);
     }
-
-    // Call this method when the enemy is killed
-    public void MarkAsKilled()
-    {
-        if (rememberKilledEnemies)
-        {
-            EnemyData.Instance.MarkAsKilled(enemyID); // Mark the enemy as killed in the enemy data
-            EnemyData.Instance.SaveEnemyData();
-        }
-    }
 }
+
+
+

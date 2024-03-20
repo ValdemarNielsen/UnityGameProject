e987b2a6dfdb5fce59ed333d191747e123432c7b
seburu <sebynoah@gmail.com>
2024-03-20 18:51:42 +0100

    .

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using GameProject.Models;
 using System.Collections;
 using System.Collections.Generic;
@@ -31,4 +32,42 @@ public static class GameManager
         return new int[] { GameManager.playerRowHolder, GameManager.playerColHolder };
     }
 
+=======
+using GameProject.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public static class GameManager
+{
+    public static Maze MazeHolder { get; set; }
+
+    public static int playerRowHolder { get; set; }
+
+    public static int playerColHolder { get; set; }
+
+    public static Vector2 spawnPoint { get; set; }
+    public static int[,] HasVisitedRoom { get; set; } = new int[5,5];
+
+
+    public static void UpdatePlayerPosition(int newRow, int newColumn)
+    {
+        GameManager.playerRowHolder = newRow;
+        GameManager.playerColHolder = newColumn;
+        HasVisitedRoom[newRow, newColumn] += 1;
+    }
+
+    // Check if the room has been visited
+    public static bool HasVisited(int row, int column)
+    {
+        return HasVisitedRoom[row, column] == 1;
+    }
+
+    public static int[] GetPlayerPosition()
+    {
+        return new int[] { GameManager.playerRowHolder, GameManager.playerColHolder };
+    }
+
+>>>>>>> cb456817d18890cdd7091b65c41f996aa149a297
 }

0baedbe517defdd3a9ba412dc221ec972c23fec1
Valdemar Orla Nielsen <s215798@dtu.dk>
2024-04-27 14:53:45 +0200

    new tests

--- Assets/Scripts/Tests/Editor/MazeGeneratorTests.cs
+++ Assets/Scripts/Tests/Editor/MazeGeneratorTests.cs
@@ -1,11 +1,6 @@
 using GameProject.Models;
 using GameProject.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
-using System.Threading.Tasks;
 using UnityEngine;
 
 

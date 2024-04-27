824172e74d39010b30bab2ecdde9d329ab62a0b1
Valdemar Orla Nielsen <s215798@dtu.dk>
2024-04-27 16:42:46 +0200

    made new scene with small guide and new button to go along with it.

---
+++ Assets/Scripts/UI/Buttons/IntroductionSceneButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Button IntroductionButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IntroductionButton = GetComponent<Button>();
+        IntroductionButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene("GameStateScene");
+
+    }
+
+
+}
